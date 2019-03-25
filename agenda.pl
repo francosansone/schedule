0d1b8f96bd83379d18f5097d953ee56ba86fad03
francosansone <fracosansone11@gmail.com>
2019-03-25 18:51:29 -0300

    setlog translation was started

---
+++ agenda.pl
@@ -0,0 +1,63 @@
+% consult('/home/fran/Repos/schedule/agenda.pl').
+
+agendaDeTurnosInit(AgendaDeTurnos) :-
+    AgendaDeTurnos = {[clientes,C],[turnos,T]} &
+    C = {} &
+    T = {}.
+    
+agendaDeTurnosInv(AgendaDeTurnos) :-
+    AgendaDeTurnos = {[clientes,C],[turnos,T]} & dom(C,T).
+    
+asignarTurno(AgendaDeTurnos,Name_i,Dni_i,Fecha_i,Ahora_i,AgendaDeTurnos_) :-
+    asignarTurnoOk(AgendaDeTurnos,Name_i,Dni_i,Fecha_i,Ahora_i,AgendaDeTurnos_)
+    or
+    asignarTurnoError(AgendaDeTurnos,Fecha_i,Ahora_i,AgendaDeTurnos_).
+    
+asignarTurnoOk(AgendaDeTurnos,Name_i,Dni_i,Fecha_i,Ahora_i,AgendaDeTurnos_) :-
+    asignarTurnoPrimeraVez(AgendaDeTurnos,Name_i,Dni_i,Fecha_i,Ahora_i,AgendaDeTurnos_)
+    or
+    asignarTurnoCliente(AgendaDeTurnos,Dni_i,Fecha_i,Ahora_i,AgendaDeTurnos_).
+    
+asignarTurnoPrimeraVez(AgendaDeTurnos,Name_i,Dni_i,Fecha_i,Ahora_i,AgendaDeTurnos_) :-
+    AgendaDeTurnos = {[clientes,C],[turnos,T]} &
+    dom(C,Dc) &
+    Dni_i nin Dc &
+    dom(T,Dt) &
+    Fecha_i nin Dt &
+    Fecha_i > Ahora_i &
+    un(C,{[Dni_i,Name_i]},C_) &
+    un(T,{[Fecha_i,Dni_i]},T_) &
+    AgendaDeTurnos_ = {[clientes,C_],[turnos,T_]}.
+    
+asignarTurnoCliente(AgendaDeTurnos,Dni_i,Fecha_i,Ahora_i,AgendaDeTurnos_) :-
+    AgendaDeTurnos = {[clientes,C],[turnos,T]} &
+    dom(C,Dc) &
+    Dni_i in Dc &
+    Fecha_i nin Dt &
+    Fecha_i > Ahora_i &
+    un(T,{[Fecha_i,Dni_i]},T_) &
+    AgendaDeTurnos_ = {[clientes,C],[turnos,T_]}.
+    
+asignarTurnoError(AgendaDeTurnos,Fecha_i,Ahora_i,AgendaDeTurnos_) :-
+    errorFechaPasada(AgendaDeTurnos,Fecha_i,Ahora_i,AgendaDeTurnos_)
+    or
+    turnoYaAsignado(AgendaDeTurnos,Fecha_i,AgendaDeTurnos_).
+    
+errorFechaPasada(AgendaDeTurnos,Fecha_i,Ahora_i,AgendaDeTurnos_) :-
+    AgendaDeTurnos = {_} &
+    Fecha_i =< Ahora_i &
+    AgendaDeTurnos_ = AgendaDeTurnos.
+    
+turnoYaAsignado(AgendaDeTurnos,Fecha_i,AgendaDeTurnos_) :-
+    AgendaDeTurnos = {[turnos,T] / _} &
+    dom(T,Dt) &
+    Fecha_i in Dt &
+    AgendaDeTurnos_ = AgendaDeTurnos.
+
+busquedaPorDni(AgendaDeTurnos,Dni_i,Resp_o,AgendaDeTurnos_) :-
+    AgendaDeTurnos = {[turnos,T] / _} &
+    dom(T,Dt) &
+    Dni_i in Dt &
+    dres(T,{Dni_i},TurnosTemp) &
+    dom(TurnosTemp, Resp_o) &
+    AgendaDeTurnos = AgendaDeTurnos_.
